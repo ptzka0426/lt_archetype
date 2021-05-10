d2b48c56411dea6448df42377404c3e1dda49d9d
ptka0426 <longtao0426@163.com>
2021-05-10 10:17:12 +0800

    提交

--- src/main/java/com/lt/GupaoMojo.java
+++ src/main/java/com/lt/GupaoMojo.java
@@ -5,6 +5,9 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.util.List;
 
 /**
  * @Author LT
@@ -12,8 +15,18 @@ import org.apache.maven.plugins.annotations.Mojo;
  */
 @Mojo(name = "longtao",defaultPhase = LifecyclePhase.PACKAGE)
 public class GupaoMojo extends AbstractMojo {
+
+    @Parameter
+    private String msg;
+    @Parameter
+    private List<String> options;
+/*
+*  mvn clean install   装载
+*   mvn help:system  查看系统参数
+* */
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("龙滔 骨架测试-longtao");
+        System.out.println("龙滔 骨架测试-longtao1"+msg);
+        System.out.println("龙滔 骨架测试-longtao2"+options);
     }
 }
